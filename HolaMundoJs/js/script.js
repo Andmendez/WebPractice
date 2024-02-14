c5361d9eb92f5b986f21dc68b7f258a3ce5f9476
Andres Felipe Forero Mendez <andmendezfel@gmail.com>
2024-02-13 22:04:00 -0500

    Basic

---
+++ HolaMundoJs/js/script.js
@@ -0,0 +1,14 @@
+
+console.log('Imprime en console');
+
+//VARIABLES
+
+//const : Nos permite declarar variables inmutables, osea no se puede cambiar.
+//var y let : Nos permiten crear variables mutables que si se puede cambiar
+
+const name = 'Variable';
+let apellido;
+
+apellido = 'Cambiable';
+
+console.log('My name is', name + apellido);
