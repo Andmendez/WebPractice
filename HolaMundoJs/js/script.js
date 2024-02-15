79b8c79bedc6d1e765a17ad7cc0df3e1c670c680
Andres Felipe Forero Mendez <andmendezfel@gmail.com>
2024-02-14 19:45:03 -0500

    Pruebas

--- HolaMundoJs/js/script.js
+++ HolaMundoJs/js/script.js
@@ -18,11 +18,27 @@ let numero = 1;
 let texto = "Hola!";
 let boolean = true;
 let indefinido;
+let nulo = null;
 
 console.log(typeof(numero));
 console.log(typeof(texto));
 console.log(typeof(boolean));
 console.log(typeof(indefinido));
+console.log(typeof(nulo));
+
+var boton = document.querySelector(".botonOne");
+var contador = 0;
+boton.addEventListener("click", function() {
+    
+    contador++;
+    console.log("Clickeaste el boton", contador, "veces!");
+
+    if (contador >= 10) {
+        console.log("Inicia de cero jajajaja :)")
+        contador = 0
+    }
+    
+});
 
 
 
